845dbfda62d95f4362cccab4f0902e553bc9a575
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-25 12:32:07 +0900

    feat: navbar 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import NavBar from './components/commons/NavBar/NavBar';
 import './styles/reset.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/commons/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './NavBar.module.scss';
+
+function NavBar() {
+  return <header className={styles.header}>Shield Maker</header>;
+}
+
+export default NavBar;
