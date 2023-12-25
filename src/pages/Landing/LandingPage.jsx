b5054211958eba368d967f27039cdabb79229263
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-25 19:36:38 +0900

    feat: 버튼 컴포넌트 구현

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './LandingPage.module.scss';
 import Landing1 from '../../assets/landing1.png';
+import Button from '../../components/commons/Button/Button';
 
 function LandingPage() {
   return (
@@ -19,6 +20,14 @@ function LandingPage() {
           <img className={styles.exImg} src={Landing1} alt="예시1" />
         </div>
       </article>
+      <div className={styles.btnList}>
+        <Button btnTheme="primary" btnSize="large2small">
+          직접 만들기
+        </Button>
+        <Button btnTheme="primary" btnSize="large2small">
+          JSON으로 만들기
+        </Button>
+      </div>
     </main>
   );
 }
