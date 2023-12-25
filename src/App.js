73170adf85cad96c5d473571e4f941c5e4855a75
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-25 19:13:08 +0900

    feat: LandingPage 컴포넌트 초안 작성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Dropdown from './components/commons/Dropdown/Dropdown';
 import NavBar from './components/commons/NavBar/NavBar';
+import LandingPage from './pages/Landing/LandingPage';
 import './styles/reset.scss';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Dropdown />
+      <LandingPage />
     </div>
   );
 }
--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styles from './LandingPage.module.scss';
+import Landing1 from '../../assets/landing1.png';
 
 function LandingPage() {
-  return <div></div>;
+  return (
+    <main className={styles.main}>
+      <article className={styles.article}>
+        <section className={styles.section}>
+          <h1 className={styles.slogan}>
+            누구나 손쉽게, 기술 스택 뱃지를 만들 수 있어요
+          </h1>
+          <h2 className={styles.description}>
+            기술 스택 만들기 귀찮은 당신!
+            <br /> 한번 사용해 보세요
+          </h2>
+        </section>
+        <div>
+          <img className={styles.exImg} src={Landing1} alt="예시1" />
+        </div>
+      </article>
+    </main>
+  );
 }
 
 export default LandingPage;
