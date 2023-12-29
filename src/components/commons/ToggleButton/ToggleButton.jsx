a95bba57b2537ebd4958fe8b3b265bcc74810666
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-29 16:48:03 +0900

    feat: 토글버튼 컴포넌트 구현

--- src/components/commons/ToggleButton/ToggleButton.jsx
+++ src/components/commons/ToggleButton/ToggleButton.jsx
@@ -1,7 +1,29 @@
 import styles from './ToggleButton.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 function ToggleButton() {
-  return <div></div>;
+  const [selected, setSelected] = useState('form');
+  return (
+    <div className={styles.container}>
+      <button
+        onClick={() => setSelected('form')}
+        className={clsx(
+          styles.button,
+          selected === 'form' ? styles.selected : false
+        )}>
+        입력
+      </button>
+      <button
+        onClick={() => setSelected('preview')}
+        className={clsx(
+          styles.button,
+          selected === 'preview' ? styles.selected : false
+        )}>
+        미리보기
+      </button>
+    </div>
+  );
 }
 
 export default ToggleButton;
